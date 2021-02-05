b2dd13ae21714956ee0ec378e134147f3183a500
Troy <232235382@qq.com>
2021-02-05 22:27:56 +0800

    Initial Commit

---
+++ Shared/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by Troy Jiang on 2021/2/5.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
